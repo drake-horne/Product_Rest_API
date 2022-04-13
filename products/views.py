9ad537bedaf066b6634380453993734c699503f3
Drake Horne <drakehorne@Drakes-MacBook-Air.local>
2022-04-13 14:59:53 -0700

    Created urls file in app and created the url paths and made them compatible with the url file in the product_rest_api. Also creted views functions to be able to have Get,POst, etc

--- products/views.py
+++ products/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def products_list(request):
+
+    if request.method == 'GET':
+        cars = Product.objects.all()
+        serializer = ProductSerializer(cars, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)       
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    car = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(car)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(car, data=request.data) 
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) 
